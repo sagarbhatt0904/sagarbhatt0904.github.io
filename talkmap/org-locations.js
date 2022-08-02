dfc7746ad2c2151ec8d8bf965db813a1e3d6967e
sagarbhatt0904 <sagar.bhatt0904@gmail.com>
2022-08-02 18:40:53 -0400

    fixed talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Troy, New York",
+    42.7284117,
+    -73.6917878
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Portsmouth, New Hampshire",
+    43.0702223,
+    -70.7548621
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Austin, Texas",
+    30.2711286,
+    -97.7436995
   ]
 ];
